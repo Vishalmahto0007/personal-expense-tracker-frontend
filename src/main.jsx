307224e57aba4935e572317c5fa3c28fee470948
Vishal Mahto <vishalmahto0007@gmail.com>
2025-06-18 11:52:29 +0530

    Implement lazy loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store/index";
+import LoadingSpinner from "./components/LoadingSpinner";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Suspense fallback={<LoadingSpinner />}>
+        <App />
+      </Suspense>
     </Provider>
   </React.StrictMode>
 );
